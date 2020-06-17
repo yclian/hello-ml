ecbdf02f12628552eb5b3710dcae57aafac7958b
yclian <foss@yclian.com>
2020-06-17 23:46:27 +0800

    Initial commit with ETS decompose.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hello Statistics! 
+
+Documenting my knowledge along the way of learning some mathetmatics towards a better trained machine learning practitioner.
+
+## Bootstrap
+
+Kick off this project with:
+
+    $ pipenv lock
+    $ pipenv install matplotlib pandas statsmodels
+
+Execute with 
+
+    $ pipenv shell
+
+## Plotting in WSL
+
+If you are plotting this in VS Code through WSL, you have to run your Python code in the interactive window (`ipykernel` needed).
---
+++ forecasting-ets-decompose.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot
+from pandas.core.tools.numeric import to_numeric
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+df = pd.read_csv('data/udacity/07-02-13 Bookings.csv', skiprows=0)
+
+df['Month'] = pd.to_datetime(df['Month'], format='%b').dt.month
+df['Date'] = pd.to_datetime(df[['Year', 'Month']].assign(DAY=1), format='%Y%b%d')
+df.head()
+data = df[['Date', 'Bookings']]
+data = data.set_index('Date')
+
+result = seasonal_decompose(data, model='additive', freq=12)
+print(result.trend)
+print(result.seasonal)
+print(result.resid)
+print(result.observed)
+
+result.plot()
+pyplot.show()
